11f0acb22374066b6f2a70903c030e67f14ec0c4
Jen Smith <jennifer@home>
2011-04-02 19:14:11 +0100

    Got first 2009 post working

--- Rakefile
+++ Rakefile
@@ -4,5 +4,11 @@ namespace :site do
 	task :generate do
 		sh "jekyll --pygments"
 	end
+	task :background do
+		sh "jekyll --auto --pygments"
+	end
+	task :run do
+		sh "node _node/file-server.js"
+	end
 end
 
--- _node/file-server.js
+++ _node/file-server.js
@@ -8,26 +8,33 @@ var dir = process.argv[2] || './_site';
 var port = parseInt(process.argv[3]) || 8080;
 sys.log('Serving files from ' + dir + ', port is ' + port);
 
-http.createServer(function(request, response) {
-	    var uri = url.parse(request.url).pathname;
-			if(uri==="/"){
-				uri = "/index.html";
-			}
-			    var filename = path.join(process.cwd(), dir, uri);
-					
-					sys.log("Sending " + filename);
-					    path.exists(filename, function(exists) {
-								    	if(exists) {
-												    	    fs.readFile(filename, function(err, data) {
-																				response.writeHead(200);
-																				    		response.end(data);
-																									    });
-																	    	} else {
-																						    sys.log('File not found: ' + filename);
-																								    	    response.writeHead(404);
-																													    	    response.end();
-																																			}
-											    });
-}).listen(port);
+var filePathFromRequest = function(request){
+	var uri = url.parse(request.url).pathname;
+	if(uri==="/"){
+		uri = "/index.html";
+	}
+ 	uri = 	uri.replace("%20", " "); // hackety hack
+	var filename = path.join(process.cwd(), dir, uri);
+	return filename;
+}
+
+var serverProc = function(request, response) {
+	var filename = filePathFromRequest(request);
+	sys.log("Sending " + filename);
+	path.exists(filename, function(exists) {
+				if(exists) {
+					fs.readFile(filename, function(err, data) {
+					response.writeHead(200);
+					response.end(data);
+					});
+		  	} else {
+					sys.log('File not found: ' + filename);
+					response.writeHead(404);
+					response.end();
+					}
+	});
+}
+
+http.createServer(serverProc).listen(port);
 
 sys.log("Server running at http://localhost:" + port);
